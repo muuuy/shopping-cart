e4694a2f3aa090de04e1c59003b93a7b64f9958d
Matthew Yu <myu2@umbc.edu>
2024-04-16 10:07:34 -0400

    continued working on navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,43 @@
-import styles from './Navbar.module.css'
+import { useState } from "react";
+
+import styles from "./Navbar.module.css";
+
+import { BsSearch } from "react-icons/bs";
+import { BsCart4 } from "react-icons/bs";
 
 const Navbar = () => {
-    return (
-        <>
-        <nav className={styles.nav_container}>
-            <a className={styles.logo}>Afterlife</a>
-          
-                <ul className={styles.nav_list}>
-                    <li>Home</li>
-                    <li>
-                        <a>Shop</a>
-                        <div className={styles.dropdown}>
-                            <a>temp</a>
-                            <a>temp</a>
-                        </div>
-                    </li>
-                </ul>
-        </nav>
-        </>
-    )
-}
+  const [letterVisible, setLetterVisible] = useState({});
+
+  return (
+    <>
+      <nav className={styles.nav_container}>
+        <a className={styles.logo}>Afterlife</a>
+        <div className={styles.search_container}>
+          <form className={styles.search}>
+            <input type="text" id={styles.search_bar}></input>
+            <button type="submit" id={styles.search_button}>
+              <BsSearch />
+            </button>
+          </form>
+        </div>
+        
+        <ul className={styles.nav_list}>
+          <li className={styles.list_item}>
+            <a className={styles.list_link}>Home</a>
+          </li>
+          <li className={styles.list_item}>
+            <a className={styles.list_link}>Shop</a>
+            <div className={styles.dropdown}>
+              <a>Men&apos;s Clothing</a>
+              <a>Women&apos;s Clothing</a>
+              <a>Electronics</a>
+              <a>Jewlery</a>
+            </div>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
 
 export default Navbar;
