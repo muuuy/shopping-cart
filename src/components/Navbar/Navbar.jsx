5558cf0dc4712915c559be2f616492e3daa3a1c4
Matthew Yu <myu2@umbc.edu>
2024-04-17 11:38:33 -0400

    styles navbar some more

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,23 +8,23 @@ import { BsCart4 } from "react-icons/bs";
 const Navbar = () => {
   const [letterVisible, setLetterVisible] = useState({});
 
+  const addSearchBorder = () => {
+
+  }
+
   return (
     <>
       <nav className={styles.nav_container}>
         <a className={styles.logo}>Afterlife</a>
         <div className={styles.search_container}>
           <form className={styles.search}>
-            <input type="text" id={styles.search_bar}></input>
+            <input type="text" id={styles.search_bar} onFocus={addSearchBorder}></input>
             <button type="submit" id={styles.search_button}>
-              <BsSearch />
+              <BsSearch className={styles.search_icon} size={24} />
             </button>
           </form>
         </div>
-        
         <ul className={styles.nav_list}>
-          <li className={styles.list_item}>
-            <a className={styles.list_link}>Home</a>
-          </li>
           <li className={styles.list_item}>
             <a className={styles.list_link}>Shop</a>
             <div className={styles.dropdown}>
@@ -33,6 +33,9 @@ const Navbar = () => {
               <a>Electronics</a>
               <a>Jewlery</a>
             </div>
+          </li>
+          <li className={styles.list_item} id={styles.shopping_cart}>
+            <a><BsCart4 /></a>
           </li>
         </ul>
       </nav>
