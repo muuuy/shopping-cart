75d861ae00d22d391d37c0d51e50c8ee30677ece
Matthew Yu <myu2@umbc.edu>
2024-04-20 22:21:28 -0400

    worked on banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,13 +1,27 @@
-import styles from './Banner.module.css';
+import styles from "./Banner.module.css";
 
 const Banner = () => {
-    return (
-        <>
-        <div className={styles.banner_container}>
-            
+  return (
+    <>
+      <div className={styles.container}>
+        <h1 style={{ zIndex: "1001", margin: '32px' }}>Test</h1>
+        <div
+          style={{
+            display: "flex",
+            flexAlign: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "16%",
+          }}
+        >
+          <div className={styles.banner_container}></div>
+          <div className={styles.banner_container}></div>
+          <div className={styles.banner_container}></div>
+          <div className={styles.banner_container}></div>
         </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 
 export default Banner;
