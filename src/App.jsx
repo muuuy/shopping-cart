ca5faf0f5e3ce6b6b8a00adf0f88599806b37640
Matthew Yu <myu2@umbc.edu>
2024-05-15 10:41:25 -0400

    added a info banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,10 @@
 import "./App.css";
-import { Helmet, HelmetProvider } from "react-helmet-async";
-import Favicon from "react-favicon";
-
-import Navbar from "./components/Navbar/Navbar";
-import SaleBanner from "./components/SaleBanner/SaleBanner";
-import Banner from "./components/Banner/Banner";
-import ItemCard from "./components/ItemCard/ItemCard";
+import Home from './pages/Home/Home'
 
 function App() {
   return (
     <>
-      <HelmetProvider>
-        <Helmet>
-          <title>BLACK MARKET</title>
-        </Helmet>
-      </HelmetProvider>
-
-      <Favicon
-        url={
-          "https://cdn.pixabay.com/photo/2017/03/16/21/18/logo-2150297_640.png"
-        }
-      />
-      <Navbar />
-      <div className="container">
-        <SaleBanner />
-        <Banner />
-        <ItemCard />
-      </div>
+      <Home />
     </>
   );
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,68 @@
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import Favicon from "react-favicon";
+
+import Navbar from "../../components/Navbar/Navbar";
+import SaleBanner from "../../components/SaleBanner/SaleBanner";
+import Banner from "../../components/Banner/Banner";
+import ItemCard from "../../components/ItemCard/ItemCard";
+
+import controllers from "../../assets/infoBanner/game_controllers.jpg";
+
+import styles from "./Home.module.css";
+
+const InfoBanner = () => {
+  return (
+    <>
+      <div className={styles.info_banner}>
+        <img src={controllers} id={styles.info_image}></img>
+        <div className={styles.info_text}>
+          <h1>DISCOVER, BUY, PLAY: YOUR ULTIMATE VIDEO GAME MARKETPLACE</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.
+            <br></br>
+            <br></br>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const Home = () => {
+  return (
+    <div id={styles.home_page}>
+      <HelmetProvider>
+        <Helmet>
+          <title>BLACK MARKET</title>
+        </Helmet>
+      </HelmetProvider>
+
+      <Favicon
+        url={
+          "https://cdn.pixabay.com/photo/2017/03/16/21/18/logo-2150297_640.png"
+        }
+      />
+      <Navbar />
+      <InfoBanner />
+      <div className="container">
+        <SaleBanner />
+        <Banner />
+        <ItemCard />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
