c3a578aa52fb4729821c2baadecd13fd4f9ab0de
Matthew Yu <myu2@umbc.edu>
2024-06-15 09:46:03 -0400

    started working on populating cart

--- frontend/src/components/ShoppingCart.jsx
+++ frontend/src/components/ShoppingCart.jsx
@@ -5,13 +5,20 @@ import { useSelector } from "react-redux";
 const ShoppingCart = () => {
   const items = useSelector((state) => state.user.cart);
 
-  console.log("items updated");
+  const populateCart = () => {
+    return items.map((item) => (
+      <>
+        <div>
+          <img className={styles.item_image} />
+          <h2 className={styles.item_name}></h2>
+          <p className={styles.item_price}></p>
+          <p className={styles.item_quantity}></p>
+        </div>
+      </>
+    ));
+  };
 
-  return (
-    <div>
-      <div></div>
-    </div>
-  );
+  return <div>{populateCart}</div>;
 };
 
 export default ShoppingCart;
