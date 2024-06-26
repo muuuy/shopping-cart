b8a7735091809b757439ae153be04929d4af2d73
Matthew Yu <myu2@umbc.edu>
2024-06-25 20:28:47 -0400

    figured out how jest works

--- backend/__tests__/users.test.js
+++ backend/__tests__/users.test.js
@@ -1,23 +1,27 @@
+const users = require("../routes/users");
+
 const request = require("supertest");
-const bcrypt = require("bcrypt");
 const app = require("../app");
-const User = require("../models/user");
-const Cart = require("../models/cart");
+const express = require("express");
 
 const user_controller = require("../controllers/userController");
+
+app.use(express.urlencoded({ extended: false }));
+app.use("/", users);
 
 describe("POST /user/signup", () => {
   test("should create a new user", async () => {
     const userInfo = {
       username: "testuser",
       password: "testpassword",
+      verifyPassword: "testpassword",
       email: "test@example.com",
     };
 
     await request(app)
-      .post("/user/signup/")
+      .post("/users/signup/")
       .send(userInfo)
-      .expect(404) 
+      .expect(200)
       .then((response) => {
         console.log("Response Body:", response.request.url);
       });
